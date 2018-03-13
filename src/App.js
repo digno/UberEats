bb6cd952f4df87d500a375cda7e4829a10664994
Artyom Nazarov <artyom.itah@gmail.com>
2018-03-13 20:02:00 +0300

    Made Header for Mobile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import Header from './Header';
+
 const Wrapper = styled.div`
   position: relative;
 `;
 
 class App extends Component {
   render() {
-    return <Wrapper />;
+    return (
+      <Wrapper>
+        <Header />
+      </Wrapper>
+    );
   }
 }
 
---
+++ src/Header/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import logo from './logo.svg';
+
+const Header = styled.header`
+  background: #ffffff;
+  box-shadow: 0px 0px 16px rgba(38, 38, 38, 0.16);
+  padding: 16px 0;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.a``;
+
+const Buttons = styled.div``;
+
+const Button = styled.button`
+  padding: 14px 15.5px;
+  border-radius: 2px;
+  border: none;
+  cursor: pointer;
+  font-weight: 500;
+  border: 1px solid inherit;
+  font-size: 11px;
+`;
+
+const SignIn = Button.extend`
+  border: 1px solid #e0e0e0;
+  background-color: #fff;
+  color: #1d1d1d;
+  margin-right: 12px;
+`;
+
+const Register = Button.extend`
+  color: #ffffff;
+  background: #262626;
+`;
+
+export default () => {
+  return (
+    <Header>
+      <div className="container">
+        <Content>
+          <Logo href="#">
+            <img src={logo} alt="logo" />
+          </Logo>
+
+          <Buttons>
+            <SignIn>Sign In</SignIn>
+            <Register>Register</Register>
+          </Buttons>
+        </Content>
+      </div>
+    </Header>
+  );
+};
